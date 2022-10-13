a38defd812327e5136feb17915a88e088c18d09b
Anshuman Goel <99547341+AnshumanGoel@users.noreply.github.com>
2022-10-13 15:04:53 +0530

    Create Sqrt(x).cpp

---
+++ Sqrt(x).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  cout << "Enter x : ";
+  int x;
+  cin>>x;
+  // print the square root of x
+  cout << sqrt(x);
+
+  return 0;
+}
